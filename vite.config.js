ea28d94d58e3fde5c64fae1ad2625b9db795ed83
Ian Eduardo Bravo Pineda <me@ianpineda.com>
2026-05-13 20:20:30 -0600

    Add conditional base URL so that the site works both in prod and dev

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [svelte()],
-})
+  base: mode === 'production' ? '/luz_y_gracia_website/' : '/',
+}))
